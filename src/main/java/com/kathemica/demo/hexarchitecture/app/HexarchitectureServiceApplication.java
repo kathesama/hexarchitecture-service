527b6e05162cc53d9ce395b38cd906f960c3f7ff
kathemica <kathemica@gmail.com>
2024-03-13 21:01:29 -0300

    adding sources

---
+++ src/main/java/com/kathemica/demo/hexarchitecture/app/HexarchitectureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kathemica.demo.hexarchitecture.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexarchitectureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexarchitectureServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kathemica/demo/hexarchitecture/app/HexarchitectureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kathemica.demo.hexarchitecture.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexarchitectureServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
